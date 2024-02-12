6d78d79c60d0e231c1a72920e5b33f08f76d21f2
lonewanderer27 <adrianejames27@gmail.com>
2024-02-12 09:31:46 +0800

    Add custom tab bar icons

--- routes/tabs.tsx
+++ routes/tabs.tsx
@@ -2,16 +2,30 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStack from "./home";
 import AboutStack from "./about";
+import { TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 function TabsNav() {
     return (
         <Tab.Navigator screenOptions={{
-            headerShown: false
+            headerShown: false,
+            tabBarStyle:{
+                height: 60,
+                paddingBottom: 10
+            }
         }}>
-            <Tab.Screen name="GameZone" component={HomeStack} />
-            <Tab.Screen name="About" component={AboutStack} />
+            <Tab.Screen name="GameZone" component={HomeStack} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialIcons name="games" size={size} color={color} />
+                )
+            }} />
+            <Tab.Screen name="About" component={AboutStack} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialIcons name="info" size={size} color={color} />
+                )
+            }} />
         </Tab.Navigator>
     )
 }
